bbb3991fd9286fb290d5ef2785bf3663e89b97ba
milja02 <nikola.miljkovic1234@gmail.com>
2026-06-22 10:53:29 +0200

    Dokumentovana PolaznikJson klasa

--- src/main/java/rs/fon/json/PolaznikJson.java
+++ src/main/java/rs/fon/json/PolaznikJson.java
@@ -19,18 +19,38 @@ import com.google.gson.JsonParser;
 
 import rs.fon.domen.Polaznik;
 
+/**
+ * Čuva i učitava polaznika u JSON fajl, kao i podatke sa RandomUser Web API-ja.
+ * Za serijalizaciju i deserijalizaciju koristi Gson biblioteku.
+ *
+ * @author Nikola Miljković
+ */
 public class PolaznikJson {
 
+    /** URL RandomUser API-ja za učitavanje nasumičnog polaznika. */
     private static final String RANDOM_USER_API_URL = "https://randomuser.me/api/?nat=rs&inc=name,cell";
 
+    /** Gson parser. */
     private final Gson gson;
+
+    /** HTTP klijent za slanje GET zahteva ka Web servisu. */
     private final HttpClient httpClient;
 
+    /**
+     * Konstruktor koji kreira Gson parser i HTTP klijent.
+     */
     public PolaznikJson() {
         gson = new GsonBuilder().setPrettyPrinting().create();
         httpClient = HttpClient.newHttpClient();
     }
 
+    /**
+     * Serijalizuje polaznika u JSON i upisuje ga u fajl.
+     *
+     * @param polaznik polaznik za čuvanje
+     * @param putanjaFajla putanja do JSON fajla
+     * @throws IOException ako upis u fajl ne uspe
+     */
     public void sacuvaj(Polaznik polaznik, String putanjaFajla) throws IOException {
         Path putanja = Path.of(putanjaFajla);
         try (Writer writer = Files.newBufferedWriter(putanja, StandardCharsets.UTF_8)) {
@@ -38,6 +58,13 @@ public class PolaznikJson {
         }
     }
 
+    /**
+     * Deserijalizuje polaznika iz JSON fajla.
+     *
+     * @param putanjaFajla putanja do JSON fajla
+     * @return učitani polaznik
+     * @throws IOException ako čitanje fajla ne uspe
+     */
     public Polaznik ucitaj(String putanjaFajla) throws IOException {
         Path putanja = Path.of(putanjaFajla);
         try (Reader reader = Files.newBufferedReader(putanja, StandardCharsets.UTF_8)) {
@@ -45,12 +72,28 @@ public class PolaznikJson {
         }
     }
 
+    /**
+     * Šalje GET zahtev RandomUser API-ju i vraća novog polaznika
+     * sa popunjenim imenom, prezimenom i brojem telefona.
+     *
+     * @return polaznik sa podacima sa API-ja
+     * @throws IOException ako mrežna komunikacija ne uspe
+     * @throws InterruptedException ako se zahtev prekine
+     */
     public Polaznik ucitajNasumicnogPolaznika() throws IOException, InterruptedException {
         Polaznik polaznik = new Polaznik();
         popuniNasumicnogPolaznika(polaznik);
         return polaznik;
     }
 
+    /**
+     * Preuzima nasumičnog korisnika sa RandomUser API-ja i postavlja
+     * ime, prezime i broj telefona na postojeći objekat.
+     *
+     * @param polaznik polaznik koji se popunjava
+     * @throws IOException ako komnikacija ne uspe
+     * @throws InterruptedException ako se zahtev prekine
+     */
     public void popuniNasumicnogPolaznika(Polaznik polaznik) throws IOException, InterruptedException {
         HttpRequest zahtev = HttpRequest.newBuilder().uri(URI.create(RANDOM_USER_API_URL)).GET().build();
 
@@ -63,6 +106,12 @@ public class PolaznikJson {
         obradiOdgovor(odgovor.body(), polaznik);
     }
 
+    /**
+     * Parsira JSON odgovor Web servisa i postavlja tražena polja na polaznika.
+     *
+     * @param jsonOdgovor telo HTTP odgovora
+     * @param polaznik polaznik koji se popunjava
+     */
     private void obradiOdgovor(String jsonOdgovor, Polaznik polaznik) {
         JsonObject koren = JsonParser.parseString(jsonOdgovor).getAsJsonObject();
         JsonArray rezultati = koren.getAsJsonArray("results");
